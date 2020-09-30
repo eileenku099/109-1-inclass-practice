b7febf35cd366bfafa526c33e8187ca031459e99
eileenku099 <71368132+eileenku099@users.noreply.github.com>
2020-09-30 15:03:46 +0800

    Add files via upload

---
+++ 0930.R
@@ -0,0 +1,45 @@
+a <- 2
+a = 2
+3 -> b
+
+my_108_total_credits <- 15
+_108_total_credits <- 15
+108_total_credits <- 15
+_my_108_total_credits <- 15
+my.108.total_credits <- 15
+.108.total_credits <- 15
+.my.108.total_credits <- 15
+我的108學年總學分數 <- 15
+`我的108學年總學分數` <- 15
+`.108.total_credits` <- 15
+
+a <- 15 
+b <- a
+c <- y
+
+'我的年紀 <- 20
+my_age <- 我的年紀
+
+num1 <- 5
+vNum1 <- c(2,5)
+vNum2 <- c(2, num1)
+num1 <- 5
+num2 <- c(5)
+vNum1 <- c(-1,5)
+vNum2 <- c(2,3)
+vNum3 <- c(vNum1,vNum2)
+vNum4 <- c(-1,5,2,3)
+vNum5 <- c(c(-1,5),c(2,3))
+
+vNum1 <- c(-1,5)
+vNum2 <- c(2,3)
+vNum4 <- c()
+
+vNum3 <- c(vNum2, vNum1)
+vNum4 <- c(vNum4, vNum1)
+
+vNum3 <- c(vNum2, vNum1)
+vNum4 <- c(vNum4, vNum1) 
+
+vNum3 <- c(vNum2, vNum1)
+vNum4 <- c(vNum4, vNum1)
